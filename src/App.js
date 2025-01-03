3320c60a8ff90d36ca2230b1449f65eef38952f0
Divya <divyabaheti101@gmail.com>
2025-01-03 16:43:26 +0530

    Router and Link

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { Link, Router } from 'react-router-dom';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to ='/'>Home</Link>
+              </li>
+              <li to='/about'>About</li>
+            </ul>
+          </nav>
+        </div>
+        
+      </Router>
     </div>
   );
 }
