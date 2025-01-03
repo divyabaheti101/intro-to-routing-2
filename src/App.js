aefda9a18741b47a39c682357f2f80cbbea7eaff
Divya <divyabaheti101@gmail.com>
2025-01-03 16:54:37 +0530

    Browser Router, Routes, Route, point to component to be routed to

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
-import { Link, Router } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <Router>
+      <BrowserRouter>
         <div>
           <nav>
             <ul>
               <li>
                 <Link to ='/'>Home</Link>
               </li>
-              <li to='/about'>About</li>
+              <li>
+                <Link to='/about'>About</Link>
+              </li>
             </ul>
           </nav>
+
+          <Routes>
+            <Route path='/about' element={<About />} />
+            <Route path='/' element={<Home />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+
         </div>
-        
-      </Router>
-    </div>
+      </BrowserRouter>
   );
+}
+
+function Home(){
+  return (
+    <h2>Home</h2>
+  )
+}
+
+function About(){
+  return (
+    <h2>About</h2>
+  )
+}
+
+function NotFound(){
+  return (
+    <h2>404 Not Found</h2>
+  )
 }
 
 export default App;
